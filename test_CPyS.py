6e5443b80ba590f10b03cc1d463b87d486c56a47
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-09-11 10:46:21 +0200

    Add test for theta_track

--- test_CPyS.py
+++ test_CPyS.py
@@ -1,4 +1,10 @@
-from CPyS.theta import theta
+import pytest
+
+from CPyS.theta import theta, theta_track
+from CPyS.B import B, B_vector
+
+import pandas as pd
+import xarray as xr
 
 def test_theta():
     x0, x1, y0, y1 = 0, 1, 0, 0 # Eastward
@@ -10,3 +16,9 @@ def test_theta():
     x0, x1, y0, y1 = 0, 0, 0, -1 # Southward
     assert theta(x0,x1,y0,y1) == 270
     
+def test_theta_track():
+    lon = [0,1,1,0,0]
+    lat = [0,0,1,1,0]
+    t = theta_track(lon, lat)
+    assert len(t) == len(lon)
+    assert t == pytest.approx([0, 90, 180, 270, 270], 0.01)
